f5985a47bd110126c2e487b5ca54d3b3c152cb30
CrisThegGreat1 <crnivorxdd1234@gmail.com>
2026-09-06 21:39:07 +0000

    Complete Lab 1

---
+++ Lab 1/hello_world.py
@@ -0,0 +1,4 @@
+# Name: Cristofer Julio Rojas Soto
+#This program will be a welcome message and prints into the terminal
+message = " Another day another A" # adds a string to message
+print(message)# prints the value of the varaiable 
